ffebb70c571324977774a63bcfc16bf3577fa5d4
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-06 22:52:39 +0500

    26th  Assignment

---
+++ main.ts
@@ -0,0 +1,26 @@
+//Assignment 26
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// • If the alien’s color is green
+
+let alienColor = "green";
+
+// print a statement that the player just earned 5 points for shooting the alien.
+
+if(alienColor === "green"){
+       console.log("You just earned 5 points for shooting the green alien!");
+}
+
+
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+else {
+    console.log("player just earned 10 points!");
+
+}
+// • Write one version of this program that runs the if block and another that runs the else block.
+
+if(alienColor === "blue"){
+    console.log("my color is blue!");
+
+} else {
+    console.log("iam from else statement!");
+}
